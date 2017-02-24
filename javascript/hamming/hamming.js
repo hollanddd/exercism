385f705e9a20fd2cf3bb62790b4977c50c2d5f4b
hollanddd <me@darrenholland.com>
2017-02-24 14:58:06 -0600

    hamming solution

---
+++ javascript/hamming/hamming.js
@@ -0,0 +1,13 @@
+
+var Hamming = function() {};
+
+Hamming.prototype.compute = function(one, other) {
+  if (one.length !== other.length) throw new Error('DNA strands must be of equal length.');
+  let diff = 0;
+  for (var i = 0; i < one.length; i++) {
+    if (one[i] !== other[i]) diff += 1;
+  }
+  return diff;
+}
+
+module.exports = Hamming;
