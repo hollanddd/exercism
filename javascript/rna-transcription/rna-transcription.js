bf628f1afa905c10dc11e485e248ebef1fbfdfa0
hollanddd <me@darrenholland.com>
2017-02-25 12:58:25 -0600

    i should have saved this for a wednesday

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,15 +1,15 @@
 
 let DnaTranscriber = function() {
-  this.dna_nucs = 'A,C,G,T'.split(',');
-  this.rna_nucs = 'U,G,C,A'.split(',');
+  this.dnaNucs = 'A,C,G,T'.split(',');
+  this.rnaNucs = 'U,G,C,A'.split(',');
 };
 
 DnaTranscriber.prototype.toRna = function(dnaStrand) {
   rna = '';
   [...dnaStrand].forEach(nuc => {
-    compliment = this.dna_nucs.indexOf(nuc);
+    compliment = this.dnaNucs.indexOf(nuc);
     if (compliment < 0) throw new Error('Invalid input');
-    rna += this.rna_nucs[compliment];
+    rna += this.rnaNucs[compliment];
   })
   return rna;
 }
