f708d339169dd55786bc48af6d0393488503c45b
hollanddd <me@darrenholland.com>
2017-02-27 17:14:58 -0600

    guard against change

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,15 +1,15 @@
 
-let DnaTranscriber = function() {
-  this.dnaNucs = 'A,C,G,T'.split(',');
-  this.rnaNucs = 'U,G,C,A'.split(',');
-};
+let DnaTranscriber = function() {};
 
 DnaTranscriber.prototype.toRna = function(dnaStrand) {
+  const dnaNucs = 'ACGT'.split('');
+  const rnaNucs = 'UGCA'.split('');
+
   rna = '';
   [...dnaStrand].forEach(nuc => {
-    compliment = this.dnaNucs.indexOf(nuc);
+    compliment = dnaNucs.indexOf(nuc);
     if (compliment < 0) throw new Error('Invalid input');
-    rna += this.rnaNucs[compliment];
+    rna += rnaNucs[compliment];
   })
   return rna;
 }
