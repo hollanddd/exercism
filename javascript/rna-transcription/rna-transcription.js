3effe5c8b01ded675e7ccc06e9baecfe169c1b6c
hollanddd <me@darrenholland.com>
2017-02-27 18:20:31 -0600

    polite but not complete

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -5,9 +5,9 @@ class DnaTranscriber {
     const rnaNucs = 'UGCA'.split('');
     let rna = '';
     [...dnaStrand].forEach(nuc => {
-      let compliment = dnaNucs.indexOf(nuc);
-      if (compliment < 0) throw new Error('Invalid input');
-      rna += rnaNucs[compliment];
+      let complement = dnaNucs.indexOf(nuc);
+      if (complement < 0) throw new Error('Invalid input');
+      rna += rnaNucs[complement];
     })
     return rna;
   }
