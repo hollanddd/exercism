857bd110f63f180fc4f1418288b8aba185b17379
hollanddd <me@darrenholland.com>
2017-03-15 12:08:25 -0500

    remove extra tab

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,17 +1,23 @@
 
 class Pangram {
-  constructor(phrase) {
+  constructor (phrase) {
     this.phrase = phrase
     .toLowerCase()
-    .replace(/[\"\s!?,\t\n\t]/g, '')  
+    .replace(/[\"\s!?,\n\t]/g, '')  
   }
   
-  isPangram() {
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
-    if (this.phrase.length < 1) return false;
+  isPangram () {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    
+    if (this.phrase.length < 1) {
+      return false;
+    }
+
     for (let c of this.phrase) {
       let index = alphabet.indexOf(c)
-      if (index > -1) alphabet.splice(index, 1)
+      if (index > -1) {
+        alphabet.splice(index, 1);
+      } 
     }
     return alphabet.length == 0
   }
